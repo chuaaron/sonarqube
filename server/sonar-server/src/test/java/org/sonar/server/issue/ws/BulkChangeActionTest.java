8ecd8801dfc271f0e848fcb23a22879fd6bdb8d0
Daniel Schwarz <daniel.schwarz@sonarsource.com>
2017-11-27 09:24:26 +0100

    Reduce coupling from sonar-server (test) to sonar-ws

--- server/sonar-server/src/test/java/org/sonar/server/issue/ws/BulkChangeActionTest.java
+++ server/sonar-server/src/test/java/org/sonar/server/issue/ws/BulkChangeActionTest.java
@@ -25,6 +25,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import javax.annotation.CheckForNull;
 import javax.annotation.Nullable;
 import org.junit.Before;
 import org.junit.Rule;
@@ -67,10 +68,12 @@ import org.sonar.server.tester.UserSessionRule;
 import org.sonar.server.ws.TestRequest;
 import org.sonar.server.ws.WsActionTester;
 import org.sonarqube.ws.Issues.BulkChangeWsResponse;
-import org.sonarqube.ws.client.issue.BulkChangeRequest;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.collect.Lists.newArrayList;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
+import static java.util.Objects.requireNonNull;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
 import static org.mockito.Matchers.eq;
@@ -607,4 +610,137 @@ public class BulkChangeActionTest {
     actions.add(new org.sonar.server.issue.CommentAction(issueFieldsSetter));
   }
 
+  private class BulkChangeRequest {
+
+    private final List<String> issues;
+    private final String assign;
+    private final String setSeverity;
+    private final String setType;
+    private final String doTransition;
+    private final List<String> addTags;
+    private final List<String> removeTags;
+    private final String comment;
+    private final Boolean sendNotifications;
+
+    private BulkChangeRequest(Builder builder) {
+      this.issues = builder.issues;
+      this.assign = builder.assign;
+      this.setSeverity = builder.setSeverity;
+      this.setType = builder.setType;
+      this.doTransition = builder.doTransition;
+      this.addTags = builder.addTags;
+      this.removeTags = builder.removeTags;
+      this.comment = builder.comment;
+      this.sendNotifications = builder.sendNotifications;
+    }
+
+    public List<String> getIssues() {
+      return issues;
+    }
+
+    @CheckForNull
+    public String getAssign() {
+      return assign;
+    }
+
+    @CheckForNull
+    public String getSetSeverity() {
+      return setSeverity;
+    }
+
+    @CheckForNull
+    public String getSetType() {
+      return setType;
+    }
+
+    @CheckForNull
+    public String getDoTransition() {
+      return doTransition;
+    }
+
+    public List<String> getAddTags() {
+      return addTags;
+    }
+
+    public List<String> getRemoveTags() {
+      return removeTags;
+    }
+
+    @CheckForNull
+    public String getComment() {
+      return comment;
+    }
+
+    @CheckForNull
+    public Boolean getSendNotifications() {
+      return sendNotifications;
+    }
+
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  public static class Builder {
+    private List<String> issues;
+    private String assign;
+    private String setSeverity;
+    private String setType;
+    private String doTransition;
+    private List<String> addTags = newArrayList();
+    private List<String> removeTags = newArrayList();
+    private String comment;
+    private Boolean sendNotifications;
+
+    public Builder setIssues(List<String> issues) {
+      this.issues = issues;
+      return this;
+    }
+
+    public Builder setAssign(@Nullable String assign) {
+      this.assign = assign;
+      return this;
+    }
+
+    public Builder setSetSeverity(@Nullable String setSeverity) {
+      this.setSeverity = setSeverity;
+      return this;
+    }
+
+    public Builder setSetType(@Nullable String setType) {
+      this.setType = setType;
+      return this;
+    }
+
+    public Builder setDoTransition(@Nullable String doTransition) {
+      this.doTransition = doTransition;
+      return this;
+    }
+
+    public Builder setAddTags(List<String> addTags) {
+      this.addTags = requireNonNull(addTags);
+      return this;
+    }
+
+    public Builder setRemoveTags(List<String> removeTags) {
+      this.removeTags = requireNonNull(removeTags);
+      return this;
+    }
+
+    public Builder setComment(@Nullable String comment) {
+      this.comment = comment;
+      return this;
+    }
+
+    public Builder setSendNotifications(@Nullable Boolean sendNotifications) {
+      this.sendNotifications = sendNotifications;
+      return this;
+    }
+
+    public BulkChangeRequest build() {
+      checkArgument(issues != null && !issues.isEmpty(), "Issue keys must be provided");
+      return new BulkChangeRequest(this);
+    }
+  }
 }
